3fe3ae1726c5fbef712d75ef5cdbb857de68aa50
pjw960316 <pjw960316@naver.com>
2021-03-17 19:12:22 +0900

    03/17 : 2가지 기능 1. seekbar 구현완료 2. checkbox 구현 중단 -> radio-button - radio-button을 이용하면 쉽게 구현할 것 같습니다.
    <3/18>
    - radio-button 구현
    - firebase 연동 시작
    - listener 학습
    
    <3/19>
    - 배경색 , 사진 추가

--- app/src/main/java/com/example/BurningUp/AddChatRoomActivity.kt
+++ app/src/main/java/com/example/BurningUp/AddChatRoomActivity.kt
@@ -1,10 +1,15 @@
 package com.example.BurningUp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.CheckBox
+import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import com.example.BurningUp.databinding.ActivityAddChatRoomBinding
-import com.example.BurningUp.databinding.ActivityLoginBinding
 import com.google.firebase.auth.FirebaseAuth
+
 
 /* TODO :
 <UI>
@@ -28,5 +33,71 @@ class AddChatRoomActivity : AppCompatActivity()
         super.onCreate(savedInstanceState)
         mBinding = ActivityAddChatRoomBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        MakeBaseSeekBar()
+        ChangeSeekBar()
+        ChangeCheckBox()
     }
+
+    fun MakeBaseSeekBar()
+    {
+        binding.seekBar.progress = 0 //default
+        binding.seekBar.max = 20
+    }
+
+    //https://lab.cliel.com/entry/Kotlin-%EC%9C%84%EC%A0%AF-SeekBar
+    fun ChangeSeekBar()
+    {
+        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean)
+            {
+                binding.tvMaxPersonCnt.setText(binding.seekBar.progress.toString() + " 명")
+                /*
+                var cur_seekbar_value : Int
+                cur_seekbar_value = binding.seekBar.progress
+                Log.d("test" , cur_seekbar_value.toString())*/
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?)
+            {
+
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?)
+            {
+
+            }
+        })
+    }
+
+    fun ChangeCheckBox()
+    {
+        var vote_rate = 3
+        //매일 : 0
+        //매주 : 1
+        //매월 : 2
+        //Except : 3
+        binding.cbDay.setOnClickListener {
+            if(binding.cbDay.isChecked)
+            {
+                vote_rate = 0;
+                binding.cbWeek.isChecked = false;
+                binding.cbMonth.isChecked = false;
+            }
+            if(binding.cbWeek.isChecked)
+            {
+                vote_rate = 1;
+                binding.cbDay.isChecked = false;
+                binding.cbMonth.isChecked = false;
+            }
+            if(binding.cbMonth.isChecked)
+            {
+                vote_rate = 2;
+                binding.cbDay.isChecked = false;
+                binding.cbWeek.isChecked = false;
+            }
+            Log.d("test" , vote_rate.toString())
+        }
+    }
+
+
 }
