ef13e41865d7652c7d1e4c97aa43fd7b161b7989
YeJinii <yeagin98@naver.com>
2021-03-31 22:29:37 +0900

    - chatRoom UI 서랍, 뒤로가기 버튼 구현

--- app/src/main/java/com/example/BurningUp/ChatListFragment.kt
+++ app/src/main/java/com/example/BurningUp/ChatListFragment.kt
@@ -1,14 +1,20 @@
 package com.example.BurningUp
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.BurningUp.databinding.ActivityChatRoomBinding
+import kotlinx.android.synthetic.main.fragment_chatlist.view.*
 
 class ChatListFragment:Fragment() {
+
+    private var mBinding: ActivityChatRoomBinding? = null
+    private val binding get() = mBinding!!
     //채팅방 프래그먼트
     companion object{
 
@@ -22,6 +28,8 @@ class ChatListFragment:Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Log.d(TAG, "ChatFrag_oncreate() called")
+
+
     }
     //
     // 프래그먼트가 메인 액티비티에 붙게되는(?)
@@ -41,7 +49,11 @@ class ChatListFragment:Fragment() {
     ): View? {
         Log.d(TAG, "Chat0Frag_oncreate() called")
         val view=inflater.inflate(R.layout.fragment_chatlist,container,false)
-
+        view.go_to_chat_room_btn.setOnClickListener { view ->
+            Log.d("btnSetup", "Selected")
+            val intent = Intent(getActivity(), ChatRoomActivity::class.java)
+            startActivity(intent)
+        }
         return view
     }
 }
