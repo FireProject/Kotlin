6e66fe1cdeb3db0aa743300a6bd3155b438c11b3
mminyyoungii <oswds0107@gmail.com>
2021-05-09 17:45:24 +0900

    20210509

--- app/src/main/java/com/example/BurningUp/ProfileAdapter.kt
+++ app/src/main/java/com/example/BurningUp/ProfileAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
 class ProfileAdapter( val profileList: ArrayList<Friends.Companion.Info>):RecyclerView.Adapter<ProfileAdapter.CustomViewHolder>()
@@ -12,7 +13,16 @@ class ProfileAdapter( val profileList: ArrayList<Friends.Companion.Info>):Recycl
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileAdapter.CustomViewHolder {
         //리스트 아이템들을 붙이는 작업
         val view= LayoutInflater.from(parent.context).inflate(R.layout.list_itme,parent,false)
-        return CustomViewHolder(view)
+        return CustomViewHolder(view).apply {
+            //프로필 클릭처리를 위해
+            itemView.setOnClickListener {
+                val curPos:Int=adapterPosition  //현재 위치를 저장하는 변수
+                val Profile: Friends.Companion.Info =profileList.get(curPos)
+                Toast.makeText(parent.context,"이름: ${Profile.nickName}  상태메시지: ${Profile.stateMessage}",Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
 
     }
 
