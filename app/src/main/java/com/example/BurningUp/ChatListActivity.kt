94b60bdbce2ba4e27c1223ae461df3570a33c350
pjw960316 <pjw960316@naver.com>
2021-05-12 14:29:07 +0900

    05/12 : 오늘은 끝

--- app/src/main/java/com/example/BurningUp/ChatListActivity.kt
+++ app/src/main/java/com/example/BurningUp/ChatListActivity.kt
@@ -13,6 +13,9 @@ import kotlinx.android.synthetic.main.fragment_chat_list.btn_go_add_chat_room
 import kotlinx.android.synthetic.main.fragment_chat_list.rv_chatlist
 import kotlinx.android.synthetic.main.fragment_chat_list.view.*
 
+/*
+TODO : 이거는 이제 사용하지 않아요!!!
+ */
 class ChatListActivity : AppCompatActivity()
 {
     override fun onCreate(savedInstanceState: Bundle?)
@@ -22,22 +25,12 @@ class ChatListActivity : AppCompatActivity()
 
         //TODO : DB에서 읽어오면 , 동적배열로 구현하고 추가.
         val chatlist = arrayListOf(
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_1","11/15" , "a"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_2","10/15" , "b"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_3","9/15" , " 안녕하세요 안녕하세요안녕하세요 안녕하세요안녕하세요 안녕하세요안녕하세요 안녕하세요안녕하세요 안녕하세요"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_4","8/15" , "d"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_5","7/15" , "e"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_6","6/15" , "f"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_7","5/15" , "g"),
-                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_8","4/15" , "h")
+                ChatListOutlineValue(R.drawable.black_smile,"취업 준비방_1","11/15" , "a")
         )
-        Log.d("jiwon" , "ERROR_1");
+
         rv_chatlist.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false); //여기서 에러
-        Log.d("jiwon" , "ERROR_2");
         rv_chatlist.setHasFixedSize(true);
-        Log.d("jiwon" , "ERROR_3");
         rv_chatlist.adapter = ChatListAdapter(chatlist);
-        Log.d("jiwon" , "ERROR_4");
 
         MoveAddChatRoomActivity();
         ReturnMainActivity()
